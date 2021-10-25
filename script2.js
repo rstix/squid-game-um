2a87c055989171fe9ec9bd3dd2589900299fef7e
Roman Stix <rsonroad@gmail.com>
2021-10-25 16:38:55 -0600

    main page replaced

---
+++ script2.js
@@ -0,0 +1,15 @@
+let wrapper=document.getElementById('squid-wrapper');
+// setTimeout(phoneNo, 8000);
+// let no1= "8650 4006";
+
+function phoneNo() {
+    wrapper.style.display='none';
+}
+
+let head = document.getElementById('head');
+let imgLogo =document.getElementById('imgLogo');
+// setTimeout(logo, 13000);
+
+function logo() {
+    head.style.display = 'none';
+}
